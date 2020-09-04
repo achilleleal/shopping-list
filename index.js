8fb1ee3830485e257fbf23e94d4800266818878f
achilleleal <achilleleal@gmail.com>
2020-09-04 17:12:23 -0400

    created clear list and copy list buttons

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+// TODO: Copy list contents to clipboard. 
+// TODO: Be able to create a second list.
+// TODO: Search list item on Amazon.
+
+
 const input = document.querySelector('input');
-const button = document.querySelector('button');
+const addItemButton = document.querySelector('button');
 const list = document.querySelector('ol');
-const listChanger = document.getElementById('list-changer');
+const listChangerButton = document.getElementById('list-changer');
+const clearListButton = document.getElementById('clear-list-btn');
+const copyListButton = document.getElementById('copy-list-btn');
+
 
 const deleteButton = () => {
     let dltButton = document.createElement('button');
@@ -12,7 +20,7 @@ const deleteButton = () => {
 
 const changeListType = () => list.classList.toggle('unordered');
 
-const deleteItem = function() { //A regular function to be able to use 'this' referring to the button and not the global scope.
+const deleteItem = function() { //A regular function to be able to use 'this' to refer to the button and not the global scope.
     this.parentNode.remove();
 }
 
@@ -21,7 +29,7 @@ const addListItem = () => {
         let item = document.createElement('input');
         item.type = 'text';
         item.value = input.value;
-        item.classList.add('transparent')
+        item.classList.add('transparent', 'list-item')
 
         const listElement = document.createElement('li');
         listElement.appendChild(item);
@@ -37,8 +45,29 @@ const addItemAfterEnter = (event) => {
     }
 }
 
-listChanger.addEventListener('click', changeListType)
+const clearListContents = () => {
+    list.innerHTML = "";
+}
 
-button.addEventListener('click', addListItem);
+const copyListContents = () => {
+    const clipboard = document.querySelector('textarea');
+    const listItems = document.querySelectorAll('li');
+
+    for (let i = 0; i < listItems.length; i++) {
+        clipboard.value += `${listItems[i].firstChild.value} \n`
+        
+    }
+    clipboard.select();
+    document.execCommand("copy");
+    clipboard.value = '';
+}
 
 input.addEventListener('keypress', addItemAfterEnter);
+
+addItemButton.addEventListener('click', addListItem);
+
+listChangerButton.addEventListener('click', changeListType);
+
+clearListButton.addEventListener('click', clearListContents);
+
+copyListButton.addEventListener('click', copyListContents);
